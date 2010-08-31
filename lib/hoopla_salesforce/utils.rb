9b77f7128ef40d2b3020a3462a6b850a78df9acb
Mat and Trotter <dev@hoopla.net>
2010-08-31 14:07:55 -0400

    Fix class matcher to work with cron trigger classes.

--- lib/hoopla_salesforce/utils.rb
+++ lib/hoopla_salesforce/utils.rb
@@ -1,7 +1,7 @@
 module HooplaSalesforce
   module Utils
     def extract_class_name(body)
-      (match = body.match(/\bclass\s+(\w+)\s*\{\s*/)) && match[1]
+      (match = body.match(/\bclass\s+(\w+)/)) && match[1]
     end
 
     def extract_trigger_name(body)
